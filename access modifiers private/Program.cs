d0f7e66979d5a4fdc1d6baefeed0aacbcb69073d
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:05:23 +0530

    first commit

---
+++ access modifiers private/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace accessModifierPrivate
+{
+    class parent
+    {
+        private int value;
+        public void setvalue(int v)
+        {
+            value = v;
+        }
+        public int getValue()
+        {
+            return value;
+        }
+    }
+    
+    class child: parent
+    {
+        public void showValue()
+        {
+            ;
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            parent obj = new parent();
+            obj.setvalue (25);
+            Console.WriteLine("value =" + obj.getValue());
+        }
+    }
+}
